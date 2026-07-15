b681e8fa1faa900ff312c018f226ed6c9efcf85a
Henkan CI <henkan-ci@users.noreply.github.com>
2026-07-15 23:31:54 +0000

    chore: update to v1.2.0

--- Casks/henkan.rb
+++ Casks/henkan.rb
@@ -1,6 +1,6 @@
 cask "henkan" do
   version "1.2.0"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "4b13f03d4035c4d4c6ab4159e8f2ebbe3023b43db51ce1516ccb922b1717f036"
   url "https://github.com/kaanreal/henkan/releases/download/v1.2.0/Henkan_1.2.0_aarch64.dmg"
   name "Henkan"
   desc "osu!mania to Etterna/StepMania beatmap converter"
