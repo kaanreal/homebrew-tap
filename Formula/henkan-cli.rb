e8ea7239835a906fbcbcb2277503013acc933e9e
Henkan CI <henkan-ci@users.noreply.github.com>
2026-07-19 18:38:40 +0000

    chore: update to v1.3.0

--- Formula/henkan-cli.rb
+++ Formula/henkan-cli.rb
@@ -1,14 +1,14 @@
 class HenkanCli < Formula
   desc "CLI for osu!mania to Etterna/StepMania beatmap converter"
   homepage "https://github.com/kaanreal/henkan"
-  url "https://github.com/kaanreal/henkan/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "46e9b7490fcfab865780aa46e277c7b4d7c5508085fc1260001144d3a805ae26"
+  url "https://github.com/kaanreal/henkan/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "3eb2b7c49f58b81187dfdec41bc8edca82b32b6d435d71fa58a99ea759bf29a4"
   license "MIT"
   depends_on "rust" => :build
   def install
     system "cargo", "install", *std_cargo_args(path: "src-tauri"), "--bin", "henkan-cli"
   end
   test do
-    assert_match "Commands", shell_output("#{bin}/henkan-cli --help")
+    assert_match "Usage", shell_output("#{bin}/henkan-cli --help")
   end
 end
