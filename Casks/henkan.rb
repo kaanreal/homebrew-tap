44b1c23da3b262a3149a72b24ed271106525d615
Henkan CI <henkan-ci@users.noreply.github.com>
2026-09-23 20:51:06 +0000

    chore: update to v1.5.0

--- Casks/henkan.rb
+++ Casks/henkan.rb
@@ -1,7 +1,7 @@
 cask "henkan" do
-  version "1.3.0"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-  url "https://github.com/kaanreal/henkan/releases/download/v1.3.0/Henkan_1.3.0_aarch64.dmg"
+  version "1.5.0"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  url "https://github.com/kaanreal/henkan/releases/download/v1.5.0/Henkan-v1.5.0-macos.dmg"
   name "Henkan"
   desc "osu!mania to Etterna/StepMania beatmap converter"
   homepage "https://github.com/kaanreal/henkan"
--- Formula/henkan-cli.rb
+++ Formula/henkan-cli.rb
@@ -1,8 +1,8 @@
 class HenkanCli < Formula
   desc "CLI for osu!mania to Etterna/StepMania beatmap converter"
   homepage "https://github.com/kaanreal/henkan"
-  url "https://github.com/kaanreal/henkan/archive/refs/tags/v1.3.0.tar.gz"
-  sha256 "115bdc0686bafac6209e0db3c1a35a2c7ecb2853b888c8fa9757d85de48b2dff"
+  url "https://github.com/kaanreal/henkan/archive/refs/tags/v1.5.0.tar.gz"
+  sha256 "9ca8c6566c873a34922b505fe3e404a3b3f6511b3c29444fd3dd228f6368d491"
   license "MIT"
   depends_on "rust" => :build
   def install
